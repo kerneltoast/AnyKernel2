4efb7f9eb5a31d982d2fe77853b7728308e16368
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-15 02:00:58 -0700

    AK2: Remove dtbo patching hacks

--- anykernel.sh
+++ anykernel.sh
@@ -30,64 +30,8 @@ ps | grep zygote | grep -v grep >/dev/null && in_recovery=false || in_recovery=t
 . /tmp/anykernel/tools/ak2-core.sh;
 
 
-## AnyKernel install
+# Install kernel
 split_boot;
-
-
-# Mount system to get some information about the user's setup (only needed in recovery)
-if $in_recovery; then
-  umount /system;
-  umount /system 2>/dev/null;
-  mkdir /system_root 2>/dev/null;
-  mount -o ro -t auto /dev/block/bootdevice/by-name/system$slot /system_root;
-  mount -o bind /system_root/system /system;
-fi;
-
-
-# Patch dtbo.img on custom ROMs
-username="$(file_getprop /system/build.prop "ro.build.user")";
-echo "Found user: $username";
-case "$username" in
-  "android-build") user=google;;
-  *) user=custom;;
-esac;
-hostname="$(file_getprop /system/build.prop "ro.build.host")";
-echo "Found host: $hostname";
-case "$hostname" in
-  *corp.google.com|abfarm*) host=google;;
-  *) host=custom;;
-esac;
-if [ "$user" == "custom" -o "$host" == "custom" ]; then
-  if [ ! -z /tmp/anykernel/dtbo.img ]; then
-    ui_print " "; ui_print "You are on a custom ROM, patching dtbo to remove verity...";
-    if $in_recovery; then
-      # Temporarily block out all custom recovery binaries/libs
-      mv /sbin /sbin_tmp;
-      # Unset library paths
-      OLD_LD_LIB=$LD_LIBRARY_PATH;
-      OLD_LD_PRE=$LD_PRELOAD;
-      unset LD_LIBRARY_PATH;
-      unset LD_PRELOAD;
-    fi;
-    $bin/magiskboot --dtb-patch /tmp/anykernel/dtbo.img;
-    if $in_recovery; then
-      mv /sbin_tmp /sbin 2>/dev/null;
-      [ -z $OLD_LD_LIB ] || export LD_LIBRARY_PATH=$OLD_LD_LIB;
-      [ -z $OLD_LD_PRE ] || export LD_PRELOAD=$OLD_LD_PRE;
-    fi;
-  fi;
-else
-  ui_print " "; ui_print "You are on stock, not patching dtbo to remove verity!";
-fi;
-
-
-# Unmount system
-if $in_recovery; then
-  umount /system;
-  umount /system_root;
-  rmdir /system_root;
-  mount -o ro -t auto /system;
-fi;
 
 # Flash boot and dtbo
 flash_boot;
