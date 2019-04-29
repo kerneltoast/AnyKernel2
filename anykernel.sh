e4ea28319ab7d564b2555f8c92056ed35b1cc669
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-28 23:03:31 -0700

    AK2: Reconfigure to enable the hall sensor

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sultan Kernel for the Pixel 2 and Pixel 2 XL
+kernel.string=Enable Hall Sensor for the Pixel 2 and Pixel 2 XL
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -29,10 +29,11 @@ ps | grep zygote | grep -v grep >/dev/null && in_recovery=false || in_recovery=t
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-
-# Install kernel
+# Extract boot
 split_boot;
 
-# Flash boot and dtbo
+# Append command line option
+echo -n " uinput.enable_hall_ic=1" >> "$split_img/boot.img-cmdline"
+
+# Flash modified boot
 flash_boot;
-flash_dtbo;
